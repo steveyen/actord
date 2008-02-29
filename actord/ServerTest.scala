cdbad76d84e73506e3b47c3f80be7e18d1f96b8a
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-02-29 19:53:25 +0000

    test should support set calls on the same key

--- actord/ServerTest.scala
+++ actord/ServerTest.scala
@@ -9,6 +9,7 @@ class MServerTest extends TestConsoleMain {
   def suite = new TestSuite(
     ( "should be empty after creation" ::
       "should support get after set" ::
+      "should support set calls on the same key" ::
       Nil
     ).map(name => new MServerTestCase(name)):_*
   )
@@ -17,9 +18,8 @@ class MServerTest extends TestConsoleMain {
 class MServerTestCase(name: String) extends TestCase(name) {
   val m: MServer = new MServer(new immutable.TreeMap[String, MEntry])
 
-  val ea = MEntry("a", 0L, 0L, 0, new Array[Byte](0), 0L)
-  val eb = MEntry("b", 0L, 0L, 0, new Array[Byte](0), 0L)
-  val ec = MEntry("c", 0L, 0L, 0, new Array[Byte](0), 0L)
+  val ea  = MEntry("a", 0L, 0L, 0, new Array[Byte](0), 0L)
+  val ea2 = MEntry("a", 1L, 0L, 0, new Array[Byte](0), 0L)
       
   override def runTest = name match {
     case "should be empty after creation" =>
@@ -35,9 +35,18 @@ class MServerTestCase(name: String) extends TestCase(name) {
     case "should support get after set" =>
       assertEquals("first get", None, m.get("a"))
       assertEquals("first set", true, m.set(ea, false))
-      assertTrue("get after set 1", entrySame(m.get("a"), ea))
-      assertTrue("get after set 2", entrySame(m.get("a"), ea))
+      assertTrue  ("get after set 1", entrySame(m.get("a"), ea))
+      assertTrue  ("get after set 2", entrySame(m.get("a"), ea))
       assertEquals("no add after set", false, m.add(ea, false))
+
+    case "should support set calls on the same key" =>
+      assertEquals("get 0", None, m.get("a"))
+      assertEquals("set 1", true, m.set(ea, false))
+      assertTrue  ("get 1", entrySame(m.get("a"), ea))
+      assertEquals("set 2", true, m.set(ea, false))
+      assertTrue  ("get 2", entrySame(m.get("a"), ea))
+      assertEquals("set 3", true, m.set(ea2, false))
+      assertTrue  ("get 3", entrySame(m.get("a"), ea2))
   }
   
   def entrySame(aOpt: Option[MEntry], b: MEntry) =
