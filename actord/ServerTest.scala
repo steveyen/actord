a2680eccda50fa672549bb025147b9c01da4c0d9
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-02-29 19:20:15 +0000

    isExpired bug

--- actord/ServerTest.scala
+++ actord/ServerTest.scala
@@ -7,8 +7,8 @@ import scala.testing.SUnit._
 
 class MServerTest extends TestConsoleMain {
   def suite = new TestSuite(
-    ( "should be empty when created" ::
-      "should allow get after set" ::
+    ( "should be empty after creation" ::
+      "should support get after set" ::
       Nil
     ).map(name => new MServerTestCase(name)):_*
   )
@@ -22,24 +22,30 @@ class MServerTestCase(name: String) extends TestCase(name) {
   val ec = MEntry("c", 0L, 0L, 0, new Array[Byte](0), 0L)
       
   override def runTest = name match {
-    case "should be empty when created" =>
-      assertEquals(m.get("a"), None)
-      assertEquals(m.delete("a", 0L), false)
-      assertEquals(m.delete("a", 1L), false)      
-      assertEquals(m.replace(ea), false)
-      assertEquals(m.delta("a", 1L), "NOT_FOUND")
-      assertEquals(m.append(ea), false)
-      assertEquals(m.prepend(ea), false)
-      assertEquals(m.checkAndSet(ea, 0L), "NOT_FOUND")
+    case "should be empty after creation" =>
+      assertEquals(None,  m.get("a"))
+      assertEquals(false, m.delete("a", 0L))
+      assertEquals(false, m.delete("a", 1L))      
+      assertEquals(false, m.replace(ea))
+      assertEquals(false, m.append(ea))
+      assertEquals(false, m.prepend(ea))
+      assertEquals("NOT_FOUND", m.delta("a", 1L))
+      assertEquals("NOT_FOUND", m.checkAndSet(ea, 0L))
       
-    case "should allow get after set" =>
-      assertEquals(m.get("a"), None)
-      assertEquals(m.set(ea), true)
-      assertEquals(m.get("a"), Some(ea))
-      assertEquals(m.get("a"), Some(ea))           
-      assertEquals(m.add(ea), false)
-      
-    case "does SUnit work?" => 
-      assertTrue(true)
+    case "should support get after set" =>
+      assertEquals("first get", None, m.get("a"))
+      assertEquals("first set", true, m.set(ea))
+      assertTrue("get after set 1", entrySame(m.get("a"), ea))
+      assertTrue("get after set 2", entrySame(m.get("a"), ea))
+      assertEquals("no add after set", false, m.add(ea))
   }
+  
+  def entrySame(aOpt: Option[MEntry], b: MEntry) =
+    aOpt.map(a => (a.key == b.key) &&
+                  (a.flags == b.flags) &&
+                  (a.expTime == b.expTime) &&
+                  (a.dataSize == b.dataSize) &&
+                  (a.data == b.data || a.data.deepEquals(b.data)) &&
+                  (a.cid == b.cid)).
+         getOrElse(false)
 }
