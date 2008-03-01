f9882f6bf04f3afa05bd4c8d4bcde019a94e7766
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-03-01 01:16:17 +0000

    test should support checkAndSet

--- actord/ServerTest.scala
+++ actord/ServerTest.scala
@@ -13,7 +13,9 @@ class MServerTest extends TestConsoleMain {
       "should support add and replace operations" ::
       "should support delete calls of 0 expTime" ::
       "should support delta calls" ::
+      "should support checkAndSet" ::
       "simple benchmark" ::
+      "simple multithreaded benchmark" ::
       Nil
     ).map(name => new MServerTestCase(name)):_*
   )
@@ -28,107 +30,140 @@ class MServerTestCase(name: String) extends TestCase(name) with MTestUtil {
   val ea  = MEntry("a", 0L, 0L, 0, new Array[Byte](0), 0L)
   val ea2 = MEntry("a", 1L, 0L, 0, new Array[Byte](0), 0L)
   
-  override def runTest = name match {
-    case "should be empty after creation" =>
-      assertEquals(None,  m.get("a"))
-      assertEquals(false, m.delete("a", 0L, false))
-      assertEquals(None,  m.get("a"))
-      assertEquals(false, m.delete("a", 1L, false))      
-      assertEquals(None,  m.get("a"))
-      assertEquals(false, m.replace(ea, false))
-      assertEquals(None,  m.get("a"))
-      assertEquals(false, m.append(ea, false))
-      assertEquals(None,  m.get("a"))
-      assertEquals(false, m.prepend(ea, false))
-      assertEquals(None,  m.get("a"))
-      assertEquals("NOT_FOUND", m.delta("a", 1L, false))
-      assertEquals(None,  m.get("a"))
-      assertEquals("NOT_FOUND", m.checkAndSet(ea, 0L, false))
-      assertEquals(None,  m.get("a"))
-      
-    case "should support get after set" =>
-      assertEquals("get 0", None, m.get("a"))
-      assertEquals("set 1", true, m.set(ea, false))
-      assertEquals("get 1", true, entrySame(m.get("a"), ea))
-      assertEquals("get 1", true, entrySame(m.get("a"), ea))
-      assertEquals("add x", false, m.add(ea, false))
-
-    case "should support set calls on the same key" =>
-      assertEquals("get 0", None, m.get("a"))
-      assertEquals("set 1", true, m.set(ea, false))
-      assertEquals("get 1", true, entrySame(m.get("a"), ea))
-      assertEquals("set 2", true, m.set(ea, false))
-      assertEquals("get 2", true, entrySame(m.get("a"), ea))
-      assertEquals("set 3", true, m.set(ea2, false))
-      assertEquals("get 3", true, entrySame(m.get("a"), ea2))
-
-    case "should support add and replace operations" =>
-      assertEquals("get 0", None,  m.get("a"))
-      assertEquals("rep x", false, m.replace(ea2, false))
-      assertEquals("get 0", None,  m.get("a"))
-      assertEquals("add 1", true,  m.add(ea, false))
-      assertEquals("get 1", true,  entrySame(m.get("a"), ea))
-      assertEquals("add x", false, m.add(ea2, false))
-      assertEquals("get 1", true,  entrySame(m.get("a"), ea))
-      assertEquals("rep 1", true,  m.replace(ea, false))
-      assertEquals("get 1", true,  entrySame(m.get("a"), ea))
-      assertEquals("rep 2", true,  m.replace(ea2, false))
-      assertEquals("get 2", true,  entrySame(m.get("a"), ea2))
-
-    case "should support delete calls of 0 expTime" =>
-      assertEquals("get 0", None,  m.get("a"))
-      assertEquals("del 0", false, m.delete("a", 0L, false))
-      assertEquals("get 0", None,  m.get("a"))
-      assertEquals("set 1", true,  m.set(ea, false))
-      assertEquals("get 1", true,  entrySame(m.get("a"), ea))
-      assertEquals("del 1", true,  m.delete("a", 0L, false))
-      assertEquals("get x", None,  m.get("a"))
-
-    case "should support delta calls" =>
-      assertEquals("get 0", None, m.get("a"))
-      assertEquals("set 0", true, m.set(simpleEntry("a", "0"), false))
-      assertEquals("get 0", true, entrySame(m.get("a"), simpleEntry("a", "0")))
-      assertEquals("inc 1", "1",  m.delta("a", 1L, false))
-      assertEquals("get x", true, dataSame(m.get("a"), simpleEntry("a", "1")))
-      assertEquals("inc 1", "2",  m.delta("a", 1L, false))
-      assertEquals("inc 1", "3",  m.delta("a", 1L, false))
-      assertEquals("inc 1", "4",  m.delta("a", 1L, false))
-      assertEquals("get y", true, dataSame(m.get("a"), simpleEntry("a", "4")))
-      assertEquals("dec 1", "3",  m.delta("a", -1L, false))
-      assertEquals("get z", true, dataSame(m.get("a"), simpleEntry("a", "3")))
-      assertEquals("dec 1", "2",  m.delta("a", -1L, false))
-      assertEquals("dec 1", "1",  m.delta("a", -1L, false))
-      assertEquals("dec 1", "0",  m.delta("a", -1L, false))
-      assertEquals("get w", true, dataSame(m.get("a"), simpleEntry("a", "0")))
-      
-      // Test underflow.
-      //
-      assertEquals("dec 1", "0",  m.delta("a", -1L, false))
-      assertEquals("get m", true, dataSame(m.get("a"), simpleEntry("a", "0")))
-      assertEquals("dec 1", "0",  m.delta("a", -1L, false))
-      assertEquals("get n", true, dataSame(m.get("a"), simpleEntry("a", "0")))
-      
-    case "simple benchmark" =>
-      val lots = 40000
-      
-      def calc(verb: String, result: Long) =
-        "msecs to " + verb + " " + lots + " keys: " + result + 
-        "\n  " + verb + "s/sec: " + ((1000 * lots) / result)
+  override def runTest = {
+    println("test: " + name)
+    name match {
+      case "should be empty after creation" =>
+        assertEquals(None,  m.get("a"))
+        assertEquals(false, m.delete("a", 0L, false))
+        assertEquals(None,  m.get("a"))
+        assertEquals(false, m.delete("a", 1L, false))      
+        assertEquals(None,  m.get("a"))
+        assertEquals(false, m.replace(ea, false))
+        assertEquals(None,  m.get("a"))
+        assertEquals(false, m.append(ea, false))
+        assertEquals(None,  m.get("a"))
+        assertEquals(false, m.prepend(ea, false))
+        assertEquals(None,  m.get("a"))
+        assertEquals("NOT_FOUND", m.delta("a", 1L, false))
+        assertEquals(None,  m.get("a"))
+        assertEquals("NOT_FOUND", m.checkAndSet(ea, 0L, false))
+        assertEquals(None,  m.get("a"))
         
-      println(calc("set",
-                   benchMarkAvgMillis(1, 
-                     for (i <- 0 until lots)
-                       m.set(simpleEntry(i.toString, i.toString), false))))
-
-      println(calc("re-set",
-                   benchMarkAvgMillis(10, 
-                     for (i <- 0 until lots)
-                       m.set(simpleEntry(i.toString, i.toString), false))))
+      case "should support get after set" =>
+        assertEquals("get 0", None, m.get("a"))
+        assertEquals("set 1", true, m.set(ea, false))
+        assertEquals("get 1", true, entrySame(m.get("a"), ea))
+        assertEquals("get 1", true, entrySame(m.get("a"), ea))
+        assertEquals("add x", false, m.add(ea, false))
   
-      println(calc("get",
-                   benchMarkAvgMillis(10, 
-                     for (i <- 0 until lots)
-                       m.get(i.toString))))
+      case "should support set calls on the same key" =>
+        assertEquals("get 0", None, m.get("a"))
+        assertEquals("set 1", true, m.set(ea, false))
+        assertEquals("get 1", true, entrySame(m.get("a"), ea))
+        assertEquals("set 2", true, m.set(ea, false))
+        assertEquals("get 2", true, entrySame(m.get("a"), ea))
+        assertEquals("set 3", true, m.set(ea2, false))
+        assertEquals("get 3", true, entrySame(m.get("a"), ea2))
+  
+      case "should support add and replace operations" =>
+        assertEquals("get 0", None,  m.get("a"))
+        assertEquals("rep x", false, m.replace(ea2, false))
+        assertEquals("get 0", None,  m.get("a"))
+        assertEquals("add 1", true,  m.add(ea, false))
+        assertEquals("get 1", true,  entrySame(m.get("a"), ea))
+        assertEquals("add x", false, m.add(ea2, false))
+        assertEquals("get 1", true,  entrySame(m.get("a"), ea))
+        assertEquals("rep 1", true,  m.replace(ea, false))
+        assertEquals("get 1", true,  entrySame(m.get("a"), ea))
+        assertEquals("rep 2", true,  m.replace(ea2, false))
+        assertEquals("get 2", true,  entrySame(m.get("a"), ea2))
+  
+      case "should support delete calls of 0 expTime" =>
+        assertEquals("get 0", None,  m.get("a"))
+        assertEquals("del 0", false, m.delete("a", 0L, false))
+        assertEquals("get 0", None,  m.get("a"))
+        assertEquals("set 1", true,  m.set(ea, false))
+        assertEquals("get 1", true,  entrySame(m.get("a"), ea))
+        assertEquals("del 1", true,  m.delete("a", 0L, false))
+        assertEquals("get x", None,  m.get("a"))
+  
+      case "should support delta calls" =>
+        assertEquals("get 0", None, m.get("a"))
+        assertEquals("set 0", true, m.set(simpleEntry("a", "0"), false))
+        assertEquals("get 0", true, entrySame(m.get("a"), simpleEntry("a", "0")))
+        assertEquals("inc 1", "1",  m.delta("a", 1L, false))
+        assertEquals("get x", true, dataSame(m.get("a"), simpleEntry("a", "1")))
+        assertEquals("inc 1", "2",  m.delta("a", 1L, false))
+        assertEquals("inc 1", "3",  m.delta("a", 1L, false))
+        assertEquals("inc 1", "4",  m.delta("a", 1L, false))
+        assertEquals("get y", true, dataSame(m.get("a"), simpleEntry("a", "4")))
+        assertEquals("dec 1", "3",  m.delta("a", -1L, false))
+        assertEquals("get z", true, dataSame(m.get("a"), simpleEntry("a", "3")))
+        assertEquals("dec 1", "2",  m.delta("a", -1L, false))
+        assertEquals("dec 1", "1",  m.delta("a", -1L, false))
+        assertEquals("dec 1", "0",  m.delta("a", -1L, false))
+        assertEquals("get w", true, dataSame(m.get("a"), simpleEntry("a", "0")))
+        
+        // Test underflow.
+        //
+        assertEquals("dec 1", "0",  m.delta("a", -1L, false))
+        assertEquals("get m", true, dataSame(m.get("a"), simpleEntry("a", "0")))
+        assertEquals("dec 1", "0",  m.delta("a", -1L, false))
+        assertEquals("get n", true, dataSame(m.get("a"), simpleEntry("a", "0")))
+        
+      case "should support checkAndSet" =>
+        val c0 = MEntry("c", 0L, 0L, 0, new Array[Byte](0), 0L)
+        val c1 = MEntry("c", 1L, 0L, 0, new Array[Byte](0), 1L)
+        val c2 = MEntry("c", 2L, 0L, 0, new Array[Byte](0), 2L)
+  
+        assertEquals("get 00", None,     m.get("c"))
+        assertEquals("set c0", true,     m.set(c0, false))
+        assertEquals("get c0", true,     entrySame(m.get("c"), c0))
+        assertEquals("cas ca", "EXISTS", m.checkAndSet(c2, 2L, false))
+        assertEquals("get ca", true,     entrySame(m.get("c"), c0))
+        assertEquals("cas c1", "STORED", m.checkAndSet(c1, 0L, false))
+        assertEquals("get c1", true,     entrySame(m.get("c"), c1))
+        
+      case "simple benchmark" =>
+        val n = 4000
+        println(calc(n, "set",
+                     benchMarkAvgMillis(1, 
+                       for (i <- 0 until n)
+                         m.set(simpleEntry(genKey(i), i.toString), false))))
+        println(calc(n, "re-set",
+                     benchMarkAvgMillis(10, 
+                       for (i <- 0 until n)
+                         m.set(simpleEntry(genKey(i), i.toString), false))))
+        println(calc(n, "get",
+                     benchMarkAvgMillis(10, 
+                       for (i <- 0 until n)
+                         m.get(genKey(i)))))
+  
+      case "simple multithreaded benchmark" => 
+        val nOperations = 4000
+        val nThreads    = 5
+        val results     = new Array[String](nThreads)
+  
+        (0 until nThreads).map(
+          x => {
+            val t = new Thread {
+                      var resultId: Int = -1
+                      override def run = {
+                        results(resultId) = calc(nOperations, "set",
+                                              benchMarkAvgMillis(1, 
+                                                for (i <- 0 until nOperations)
+                                                  m.set(simpleEntry(genKey(i), i.toString), false)))
+                      }
+                    }
+            t.resultId = x
+            t.start
+            t
+          }
+        ).foreach(_.join)
+        
+        results.foreach(println _)
+    }
   }
 }
 
@@ -158,6 +193,20 @@ trait MTestUtil {
                   (a.cid == b.cid)).
          getOrElse(false)
          
+  def genKey(i: Int): String = { // Simple deterministic algorithm.
+    var r = new StringBuffer      
+    var a = 1
+    for (x <- i until (i + 4)) {
+      a = a * x
+      r = r.append(a)
+    }
+    r.reverse.toString
+  }
+  
+  def calc(n: Int, verb: String, result: Long) =
+    "msecs to " + verb + " " + n + " keys: " + result + 
+    "\n  " + verb + "s/sec: " + ((1000 * n) / result)
+
   def benchMark(repeat: Int, f: => Unit): List[Long] =
     (0 until repeat).map(
       i => {
