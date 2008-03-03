71566306f6393d63825f03f166a03f60b44349a0
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-03-03 20:19:40 +0000

    MServerTestClient closer to working

--- actord/ServerTestClient.scala
+++ actord/ServerTestClient.scala
@@ -13,8 +13,7 @@ import scala.testing.SUnit._
  */
 class MServerTestClient extends TestConsoleMain {
   def suite = new TestSuite(
-    ( "should know how to quit" ::
-      "should be empty for new keys" ::
+    ( "should be empty for new keys" ::
       "should have version" ::
       "should support set and get for a key" ::
       "should support delete" ::
@@ -34,72 +33,67 @@ class MServerTestClientCase(name: String) extends TestCase(name) {
     val in = new BufferedReader(new InputStreamReader(s.getInputStream))
     val out = new PrintWriter(s.getOutputStream, true)
   
-    def r(expect: String) = 
-      assertEquals(expect, in.readLine)
+    def r(expect: String) = {
+      val line = in.readLine
+      println("client received " + line)
+      assertEquals(expect, line)
+    }
   
     def w(x: String) = {
       out.write(line(x))
       out.flush
     }
       
-    def quit = {
-      w("quit")
-      Thread.sleep(500)
-      assertEquals(false, s.isConnected)
-      assertEquals(true, s.isClosed)
-    }    
-
-    name match {
-      case "should know how to quit" =>
-        quit
-
-      case "should be empty for new keys" =>
-        w("get" + skey("a"))
-        r("END")
-        w("get" + skey("a") + skey("b") + skey("c"))
-        r("END")
-        
-      case "should have version" =>
-        w("version")
-        val x = in.readLine
-        assertEquals(true, x.startsWith("VERSION "))
-        assertEquals(true, x.length > ("VERSION ".length + 1))
-       
-      case "should support set and get for a key" =>
-        w("get" + skey("a"))
-        r("END")
-        w("set" + skey("a") + " 0 0 5")
-        w("hello")
-        r("STORED")
-        w("get" + skey("a"))
-        r("VALUE a 0 5")
-        r("hello")
-        r("END")
-        w("get" + skey("a") + skey("b"))
-        r("VALUE a 0 5")
-        r("hello")
-        r("END")
-
-      case "should support delete" =>
-        w("set" + skey("a") + " 0 0 5")
-        w("hello")
-        r("STORED")
-        w("get" + skey("a"))
-        r("VALUE a 0 5")
-        r("hello")
-        r("END")
-        w("delete" + skey("a") + " 0")
-        r("DELETED")
-        w("get" + skey("a"))
-        r("END")
-        w("delete" + skey("a") + " 0")
-        r("NOT_FOUND")
-        w("get" + skey("a"))
-        r("END")
+    try {
+      name match {
+        case "should be empty for new keys" =>
+          w("get" + skey("a"))
+          r("END")
+          w("get" + skey("a") + skey("b") + skey("c"))
+          r("END")
+          
+        case "should have version" =>
+          w("version")
+          val x = in.readLine
+          assertEquals(true, x.startsWith("VERSION "))
+          assertEquals(true, x.length > ("VERSION ".length + 1))
+         
+        case "should support set and get for a key" =>
+          w("get" + skey("a"))
+          r("END")
+          w("set" + skey("a") + " 0 0 5")
+          w("hello")
+          r("STORED")
+          w("get" + skey("a"))
+          r("VALUE a 0 5")
+          r("hello")
+          r("END")
+          w("get" + skey("a") + skey("b"))
+          r("VALUE a 0 5")
+          r("hello")
+          r("END")
+  
+        case "should support delete" =>
+          w("set" + skey("a") + " 0 0 5")
+          w("hello")
+          r("STORED")
+          w("get" + skey("a"))
+          r("VALUE a 0 5")
+          r("hello")
+          r("END")
+          w("delete" + skey("a") + " 0")
+          r("DELETED")
+          w("get" + skey("a"))
+          r("END")
+          w("delete" + skey("a") + " 0")
+          r("NOT_FOUND")
+          w("get" + skey("a"))
+          r("END")
+      }
+    } finally {
+      if (!s.isClosed)
+        s.close
     }
-    
-    if (!s.isClosed)
-      s.close
   }
 
   val keyPrefix = Math.abs(new java.util.Random().nextInt & 0x0000FFF)
