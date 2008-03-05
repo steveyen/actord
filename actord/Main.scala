29fd15490540fe62793a9db79e3782884ca8aa9b
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-03-05 04:15:12 +0000

    cmd line args can now have aliases

--- actord/Main.scala
+++ actord/Main.scala
@@ -17,21 +17,25 @@ object Main
 {
   def main(args: Array[String]) {
     val flags = parseArgs(args)
+
     for ((spec, values) <- flags) {
       if (spec == errSpec) {
-        println("error: " + spec.spec + " : " + values.mkString(" ").trim)
+        println("error: " + spec.specs.mkString(" | ") + " : " + values.mkString(" ").trim)
         System.exit(1)
       }
-      if (spec.flag == "h") {
+
+      if (spec.name == "help") {
         println("actord -- simple mesh of actors\n")
         println(" version : TBD")
         println(" usage   : <java-invocation> [flags*]\n")
-        for (s <- flagSpecs)
-          println(s.spec + "\n " + s.description.split("\n").mkString("\n "))
+        for (s <- flagSpecs) {
+          println(s.specs.mkString(" | "))
+          println(" " + s.description.split("\n").mkString("\n "))
+        }
         System.exit(1)
       }
     }
-
+    
     startAcceptor(new MServer, 
                   Runtime.getRuntime.availableProcessors, 
                   11211)
@@ -57,67 +61,87 @@ object Main
   
   // ------------------------------------------------------
   
-  def parseArgs(args: Array[String]): List[Pair[FlagSpec, Array[String]]] =
-    if (args.isEmpty)
-      Nil
-    else {
-      val xs = (" " + args.mkString(" ")). // " -a 1 -b -c 2"
-                 split(" -")               // ["", "a 1", "b", "c 2"]
-      if (xs.headOption.
-             map(_.length > 0).
-             getOrElse(false))
-        List(Pair(errSpec, xs))
-      else
-        xs.drop(1).                        // ["a 1", "b", "c 2"]
-           toList.
-           map(arg => { 
-             val argParts = arg.split(" ")
-             flagSpecs.find(_.flag == argParts(0)).
-                       map(spec => if (spec.check(argParts))
-                                     Pair(spec, argParts)
-                                   else
-                                     Pair(errSpec, argParts)).
-                       getOrElse(Pair(errSpec, argParts))
-           })
-    }
+  def parseArgs(args: Array[String]): List[Pair[FlagSpec, Array[String]]] = {
+    val xs = (" " + args.mkString(" ")). // " -a 1 -b -c 2"
+               split(" -")               // ["", "a 1", "b", "c 2"]
+    if (xs.headOption.
+           map(_.trim.length > 0).
+           getOrElse(false))
+      List(Pair(errSpec, xs))
+    else
+      xs.drop(1).                        // ["a 1", "b", "c 2"]
+         toList.
+         map(arg => { 
+           val argParts = arg.split(" ")
+           val flag     = "-" + argParts(0)
+           flagSpecs.find(_.flags.contains(flag)).
+                     map(spec => if (spec.check(argParts))
+                                   Pair(spec, argParts)
+                                 else
+                                   Pair(errSpec, argParts)).
+                     getOrElse(Pair(errSpec, argParts))
+         })
+  }
 
-  case class FlagSpec(spec: String, description: String) {
-    val parts = spec.split(" ").toList
-    val flag  = parts(0).substring(1)
+  case class FlagSpec(name: String, specs: List[String], description: String) {
+    val flags = specs.map(_.split(" ")(0))
     
-    def check(argParts: Array[String]) = parts.length == argParts.length
+    def check(argParts: Array[String]) = {
+      val flag = "-" + argParts(0)
+      specs.filter(
+        spec => { 
+          val specParts = spec.split(" ")
+          specParts(0) == flag && 
+          specParts.length == argParts.length
+        }
+      ).isEmpty == false
+    }
   }
   
-  val errSpec = FlagSpec("incorrect flag or parameter", "")
+  val errSpec = FlagSpec("err", "incorrect flag or parameter" :: Nil, "")
   
   def flagSpecs = List(
-//  FlagSpec("-l <ip_addr>",
-//      "Listen on <ip_addr>; default to INDRR_ANY.\nThis is an important option to consider for security.\nBinding to an internal or firewalled network interface is suggested."),
-//  FlagSpec("-d",
-//      "Run server as a daemon."),
-//  FlagSpec("-u <username>",
-//      "Assume the identity of <username> (only when run as root)."),
-    FlagSpec("-m <num>",
-        "Use <num> MB memory max for object storage; the default is 64 MB."),
-    FlagSpec("-M",
-        "Instead of expiring items when max memory is reached, throw an error."),
-    FlagSpec("-c <num>",
-        "Use <num> max simultaneous connections; the default is 1024."),
-//  FlagSpec("-k",
-//      "Lock down all paged memory. This is a somewhat dangerous option with large caches, so consult the docs for configuration suggestions."),
-    FlagSpec("-p <num>",
-        "Listen on port <num>, the default is port 11211."),
-    FlagSpec("-r",
-        "Raise the core file size limit to the maximum allowable."),
-    FlagSpec("-h",
-        "Show the version of the server and a summary of options."),
-    FlagSpec("-v",
-        "Be verbose during the event loop; print out errors and warnings."),
-    FlagSpec("-vv",
-        "Be even more verbose; for example, also print client requests and responses.")
-//  FlagSpec("-i",
-//      "Print server and component licenses."),
-//  FlagSpec("-P <filename>",
-//      "Print pidfile to <filename>, only used under -d option.")
+//  FlagSpec("ipAddr", 
+//           "-l <ip_addr>" :: Nil,
+//           "Listen on <ip_addr>; default to INDRR_ANY.\nThis is an important option to consider for security.\nBinding to an internal or firewalled network interface is suggested."),
+    FlagSpec("limitMemory", 
+             "-m <num>" :: Nil,
+             "Use <num> MB memory max for object storage; the default is 64 MB."),
+    FlagSpec("noExpire", 
+             "-M" :: Nil,
+             "Instead of expiring items when max memory is reached, throw an error."),
+    FlagSpec("maxConn", 
+             "-c <num>" :: Nil,
+             "Use <num> max simultaneous connections; the default is 1024."),
+    FlagSpec("port", 
+             "-p <num>" :: Nil,
+             "Listen on port <num>, the default is port 11211."),
+    FlagSpec("growCore", 
+             "-r" :: Nil,
+             "Raise the core file size limit to the maximum allowable."),
+    FlagSpec("help", 
+             "-h" :: "-?" :: "--help" :: Nil,
+             "Show the version of the server and a summary of options."),
+    FlagSpec("verbose", 
+             "-v" :: Nil,
+             "Be verbose during the event loop; print out errors and warnings."),
+    FlagSpec("veryVerbose", 
+             "-vv" :: Nil,
+             "Be even more verbose; for example, also print client requests and responses.")
+//  FlagSpec("daemon", 
+//           "-d" :: Nil,
+//           "Run server as a daemon."),
+//  FlagSpec("username", 
+//           "-u <username>" :: Nil,
+//           "Assume the identity of <username> (only when run as root)."),
+//  FlagSpec("lockMem", 
+//           "-k" :: Nil,
+//           "Lock down all paged memory. This is a somewhat dangerous option with large caches, so consult the docs for configuration suggestions."),
+//  FlagSpec("licenses", 
+//           "-i" :: Nil,
+//           "Print server and component licenses."),
+//  FlagSpec("pidFile", 
+//           "-P <filename>" :: Nil,
+//           "Print pidfile to <filename>, only used under -d option.")
   )
 }
