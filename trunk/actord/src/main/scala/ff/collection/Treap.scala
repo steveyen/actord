2cb8c0c7fe2cd84b3dd57533285106276568c903
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-03-08 05:52:23 +0000

    treap type aliases

--- trunk/actord/src/main/scala/ff/collection/Treap.scala
+++ trunk/actord/src/main/scala/ff/collection/Treap.scala
@@ -22,125 +22,150 @@ import scala.collection._
  *
  * See: http://www.cs.cmu.edu/afs/cs.cmu.edu/project/scandal/public/papers/treaps-spaa98.pdf
  */
-class Treap[A <% Ordered[A], B <% AnyRef](val root: Treap[A, B]#Node)
+class Treap[A <% Ordered[A], B](val root: TreapNode[A, B])
 {
-  abstract class Node {
-    def split(s: A): (Node, Full, Node)
-
-    /**
-     * For join to work, we require that "this".keys < "that".keys.
-     */
-    def join(that: Node): Node
-
-    /**
-     * When union'ed, the values from "that" have precedence 
-     * over "this" when there are matching keys.
-     */ 
-    def union(that: Node): Node
-
-    /**
-     * When intersect'ed, the values from "that" have precedence 
-     * over "this" when there are matching keys.
-     */ 
-    def intersect(that: Node): Node
-    
-    /**
-     * Works like set-difference, as in "this" minus "that", or this - that.
-     */
-    def diff(that: Node): Node
-  }
+  type Node  = TreapNode[A, B]
+  type Full  = TreapFull[A, B]
+  type Empty = TreapEmpty[A, B]
   
+  def this() = this(TreapEmpty[A, B])
+  
+  def mkTreap(r: Node): Treap[A, B] = new Treap(r)
+  
+  def union(that: Treap[A, B]): Treap[A, B]     = mkTreap(root.union(that.root))
+  def intersect(that: Treap[A, B]): Treap[A, B] = mkTreap(root.intersect(that.root))
+  def diff(that: Treap[A, B]): Treap[A, B]      = mkTreap(root.diff(that.root))
+}
+
+abstract class TreapNode[A <% Ordered[A], B] 
+{
+  type Node  = TreapNode[A, B]
+  type Full  = TreapFull[A, B]
+  type Empty = TreapEmpty[A, B]
+
+  def split(s: A): (Node, Full, Node)
+
+  /**
+   * For join to work, we require that "this".keys < "that".keys.
+   */
+  def join(that: Node): Node
+
+  /**
+   * When union'ed, the values from "that" have precedence 
+   * over "this" when there are matching keys.
+   */ 
+  def union(that: Node): Node
+
+  /**
+   * When intersect'ed, the values from "that" have precedence 
+   * over "this" when there are matching keys.
+   */ 
+  def intersect(that: Node): Node
+  
+  /**
+   * Works like set-difference, as in "this" minus "that", or this - that.
+   */
+  def diff(that: Node): Node
+}
+
+case class TreapFull[A <% Ordered[A], B](key: A, value: B, left: TreapNode[A, B], right: TreapNode[A, B]) 
+   extends TreapNode[A, B] 
+{
   def mkFull(key: A, value: B, left: Node, right: Node): Node = 
-        Full(key, value, left, right)
+        TreapFull[A, B](key, value, left, right)
+  
+  def priority = key.hashCode
 
-  case class Full(key: A, value: B, left: Node, right: Node) extends Node {
-    def priority = key.hashCode
-
-    def split(s: A) = {
-      if (s == key) {
-        (left, this, right)
+  def split(s: A) = {
+    if (s == key) {
+      (left, this, right)
+    } else {
+      if (s < key) {
+        val (l1, m, r1) = left.split(s)
+        (l1, m, mkFull(key, value, r1, right))
       } else {
-        if (s < key) {
-          val (l1, m, r1) = left.split(s)
-          (l1, m, mkFull(key, value, r1, right))
-        } else {
-          val (l1, m, r1) = right.split(s)
-          (mkFull(key, value, left, l1), m, r1)
-        }
+        val (l1, m, r1) = right.split(s)
+        (mkFull(key, value, left, l1), m, r1)
       }
     }
-    
-    def join(that: Node): Node = that match {
-      case e: Empty => this
-      case b: Full =>
-        if (priority > b.priority)
-          mkFull(key, value, left, right.join(b))
-        else
-          mkFull(b.key, b.value, this.join(b.left), b.right)
-    }
-
-    def union(that: Node): Node = that match {
-      case e: Empty => this
-      case b: Full =>
-        if (priority > b.priority) {
-          val (l, m, r) = b.split(key)
-          if (m == null)
-            mkFull(key, value, left.union(l), right.union(r))
-          else
-            mkFull(m.key, m.value, left.union(l), right.union(r))
-        } else {
-          val (l, m, r) = this.split(b.key)
-          
-          // Note we don't use m because b (that) has precendence over this when union'ed.
-          //
-          mkFull(b.key, b.value, l.union(b.left), r.union(b.right))
-        }
-    }
-
-    def intersect(that: Node): Node = that match {
-      case e: Empty => e
-      case b: Full =>
-        if (priority > b.priority) {
-          val (l, m, r) = b.split(key)
-          val nl = left.intersect(l)
-          val nr = right.intersect(r)
-          if (m == null)
-            nl.join(nr)
-          else
-            mkFull(m.key, m.value, nl, nr)
-        } else {
-          val (l, m, r) = this.split(b.key)
-          val nl = l.intersect(b.left)
-          val nr = r.intersect(b.right)
-          if (m == null)
-            nl.join(nr)
-          else
-            mkFull(b.key, b.value, nl, nr) // The b value has precendence over this value.
-        }
-    }
-
-    def diff(that: Node): Node = that match {
-      case e: Empty => this
-      case b: Full =>
-        // TODO: Need to research alternative "diffb" algorithm from scandal...
-        //       http://www.cs.cmu.edu/~scandal/treaps/src/treap.sml
-        //
-        val (l2, m, r2) = b.split(key)
-        val l = left.diff(l2)
-        val r = right.diff(r2)
-        if (m == null)
-          mkFull(key, value, l, r)
-        else
-          l.join(r)
-    }
   }
   
-  case class Empty extends Node { 
-    def split(s: A)                 = (this, null, this)
-    def join(that: Node): Node      = that
-    def union(that: Node): Node     = that
-    def intersect(that: Node): Node = this
-    def diff(that: Node): Node      = this
+  def join(that: Node): Node = that match {
+    case e: Empty => this
+    case b: Full =>
+      if (priority > b.priority)
+        mkFull(key, value, left, right.join(b))
+      else
+        mkFull(b.key, b.value, this.join(b.left), b.right)
+  }
+
+  def union(that: Node): Node = that match {
+    case e: Empty => this
+    case b: Full =>
+      if (priority > b.priority) {
+        val (l, m, r) = b.split(key)
+        if (m == null)
+          mkFull(key, value, left.union(l), right.union(r))
+        else
+          mkFull(m.key, m.value, left.union(l), right.union(r))
+      } else {
+        val (l, m, r) = this.split(b.key)
+        
+        // Note we don't use m because b (that) has precendence over this when union'ed.
+        //
+        mkFull(b.key, b.value, l.union(b.left), r.union(b.right))
+      }
+  }
+
+  def intersect(that: Node): Node = that match {
+    case e: Empty => e
+    case b: Full =>
+      if (priority > b.priority) {
+        val (l, m, r) = b.split(key)
+        val nl = left.intersect(l)
+        val nr = right.intersect(r)
+        if (m == null)
+          nl.join(nr)
+        else
+          mkFull(m.key, m.value, nl, nr)
+      } else {
+        val (l, m, r) = this.split(b.key)
+        val nl = l.intersect(b.left)
+        val nr = r.intersect(b.right)
+        if (m == null)
+          nl.join(nr)
+        else
+          mkFull(b.key, b.value, nl, nr) // The b value has precendence over this value.
+      }
+  }
+
+  def diff(that: Node): Node = that match {
+    case e: Empty => this
+    case b: Full =>
+      // TODO: Need to research alternative "diffb" algorithm from scandal...
+      //       http://www.cs.cmu.edu/~scandal/treaps/src/treap.sml
+      //
+      val (l2, m, r2) = b.split(key)
+      val l = left.diff(l2)
+      val r = right.diff(r2)
+      if (m == null)
+        mkFull(key, value, l, r)
+      else
+        l.join(r)
   }
 }
 
+case class TreapEmpty[A <% Ordered[A], B] extends TreapNode[A, B] 
+{ 
+  def split(s: A) = (this, null, this)
+  def join(that: Node): Node      = that
+  def union(that: Node): Node     = that
+  def intersect(that: Node): Node = this
+  def diff(that: Node): Node      = this
+}
+
+// ---------------------------------------------------------
+
+object TreapTest extends Application {
+  val t = new Treap[Int, Int]
+}
