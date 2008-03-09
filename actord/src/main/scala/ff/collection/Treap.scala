456f8d29f3e662df94bafaf01109d2954db19da3
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-03-09 07:49:33 +0000

    looks like we have to pass the treap wrapper around everywhere

--- actord/src/main/scala/ff/collection/Treap.scala
+++ actord/src/main/scala/ff/collection/Treap.scala
@@ -22,23 +22,24 @@ import scala.collection._
  *
  * See: http://www.cs.cmu.edu/afs/cs.cmu.edu/project/scandal/public/papers/treaps-spaa98.pdf
  */
-class Treap[A <% Ordered[A], B](val root: TreapNode[A, B])
+class Treap[A <% Ordered[A], B <: AnyRef](val root: TreapNode[A, B])
 {
   def this() = this(TreapEmptyNode[A, B])
   
   def mkTreap(r: TreapNode[A, B]): Treap[A, B] = new Treap(r)
   
-  def union(that: Treap[A, B]): Treap[A, B]     = mkTreap(root.union(that.root))
-  def intersect(that: Treap[A, B]): Treap[A, B] = mkTreap(root.intersect(that.root))
-  def diff(that: Treap[A, B]): Treap[A, B]      = mkTreap(root.diff(that.root))
+  def union(that: Treap[A, B]): Treap[A, B]     = mkTreap(root.union(this, that.root))
+  def intersect(that: Treap[A, B]): Treap[A, B] = mkTreap(root.intersect(this, that.root))
+  def diff(that: Treap[A, B]): Treap[A, B]      = mkTreap(root.diff(this, that.root))
   
   override def toString = root.toString
 }
 
 // ---------------------------------------------------------
 
-abstract class TreapNode[A <% Ordered[A], B] 
+abstract class TreapNode[A <% Ordered[A], B <: AnyRef] 
 {
+  type T     = Treap[A, B]
   type Node  = TreapNode[A, B]
   type Full  = TreapFullNode[A, B]
   type Empty = TreapEmptyNode[A, B]
@@ -46,7 +47,7 @@ abstract class TreapNode[A <% Ordered[A], B]
   def isEmpty: Boolean
   def isLeaf: Boolean
   
-  def lookup(s: A): Node
+  def lookup(t: T, s: A): Node
 
   /**
    * Splits a treap into two treaps based on a split key "s".
@@ -56,51 +57,51 @@ abstract class TreapNode[A <% Ordered[A], B]
    * The tuple-3's left treap has keys all < s,
    * and the tuple-3's right treap has keyas all > s.
    */
-  def split(s: A): (Node, Full, Node)
+  def split(t: T, s: A): (Node, Full, Node)
 
   /**
    * For join to work, we require that "this".keys < "that".keys.
    */
-  def join(that: Node): Node
+  def join(t: T, that: Node): Node
 
   /**
    * When union'ed, the values from "that" have precedence 
    * over "this" when there are matching keys.
    */ 
-  def union(that: Node): Node
+  def union(t: T, that: Node): Node
 
   /**
    * When intersect'ed, the values from "that" have precedence 
    * over "this" when there are matching keys.
    */ 
-  def intersect(that: Node): Node
+  def intersect(t: T, that: Node): Node
   
   /**
    * Works like set-difference, as in "this" minus "that", or this - that.
    */
-  def diff(that: Node): Node
+  def diff(t: T, that: Node): Node
 }
 
 // ---------------------------------------------------------
 
-case class TreapEmptyNode[A <% Ordered[A], B] extends TreapNode[A, B] 
+case class TreapEmptyNode[A <% Ordered[A], B <: AnyRef] extends TreapNode[A, B] 
 { 
   def isEmpty: Boolean = true
   def isLeaf: Boolean  = throw new RuntimeException("isLeaf on empty treap node")
 
-  def lookup(s: A): Node          = this
-  def split(s: A)                 = (this, null, this)
-  def join(that: Node): Node      = that
-  def union(that: Node): Node     = that
-  def intersect(that: Node): Node = this
-  def diff(that: Node): Node      = this
+  def lookup(t: T, s: A): Node          = this
+  def split(t: T, s: A)                 = (this, null, this)
+  def join(t: T, that: Node): Node      = that
+  def union(t: T, that: Node): Node     = that
+  def intersect(t: T, that: Node): Node = this
+  def diff(t: T, that: Node): Node      = this
   
   override def toString = "_"
 }
 
 // ---------------------------------------------------------
 
-abstract class TreapFullNode[A <% Ordered[A], B] extends TreapNode[A, B] 
+abstract class TreapFullNode[A <% Ordered[A], B <: AnyRef] extends TreapNode[A, B] 
 {
   def key: A  
   def left: Node
@@ -113,17 +114,17 @@ abstract class TreapFullNode[A <% Ordered[A], B] extends TreapNode[A, B]
   def isEmpty: Boolean = false
   def isLeaf: Boolean  = left.isEmpty && right.isEmpty
 
-  def lookup(s: A): Node = 
+  def lookup(t: T, s: A): Node = 
     if (s == key)
       this
     else {
       if (s < key)
-        left.lookup(s)
+        left.lookup(t, s)
       else
-        right.lookup(s)
+        right.lookup(t, s)
     }    
 
-  def split(s: A) = {
+  def split(t: T, s: A) = {
     if (s == key) {
       (left, this, right)
     } else {
@@ -131,82 +132,82 @@ abstract class TreapFullNode[A <% Ordered[A], B] extends TreapNode[A, B]
         if (isLeaf)
           (left, null, this) // Optimization when isLeaf.
         else {
-          val (l1, m, r1) = left.split(s)
+          val (l1, m, r1) = left.split(t, s)
           (l1, m, mkNode(this, r1, right))
         }
       } else {
         if (isLeaf)
           (this, null, right) // Optimization when isLeaf.
         else {
-          val (l1, m, r1) = right.split(s)
+          val (l1, m, r1) = right.split(t, s)
           (mkNode(this, left, l1), m, r1)
         }
       }
     }
   }
   
-  def join(that: Node): Node = that match {
+  def join(t: T, that: Node): Node = that match {
     case e: Empty => this
     case b: Full =>
       if (priority > b.priority)
-        mkNode(this, left, right.join(b))
+        mkNode(this, left, right.join(t, b))
       else
-        mkNode(b, this.join(b.left), b.right)
+        mkNode(b, this.join(t, b.left), b.right)
   }
 
-  def union(that: Node): Node = that match {
+  def union(t: T, that: Node): Node = that match {
     case e: Empty => this
     case b: Full =>
       if (priority > b.priority) {
-        val (l, m, r) = b.split(key)
+        val (l, m, r) = b.split(t, key)
         if (m == null)
-          mkNode(this, left.union(l), right.union(r))
+          mkNode(this, left.union(t, l), right.union(t, r))
         else
-          mkNode(m, left.union(l), right.union(r))
+          mkNode(m, left.union(t, l), right.union(t, r))
       } else {
-        val (l, m, r) = this.split(b.key)
+        val (l, m, r) = this.split(t, b.key)
         
         // Note we don't use m because b (that) has precendence over this when union'ed.
         //
-        mkNode(b, l.union(b.left), r.union(b.right))
+        mkNode(b, l.union(t, b.left), r.union(t, b.right))
       }
   }
 
-  def intersect(that: Node): Node = that match {
+  def intersect(t: T, that: Node): Node = that match {
     case e: Empty => e
     case b: Full =>
       if (priority > b.priority) {
-        val (l, m, r) = b.split(key)
-        val nl = left.intersect(l)
-        val nr = right.intersect(r)
+        val (l, m, r) = b.split(t, key)
+        val nl = left.intersect(t, l)
+        val nr = right.intersect(t, r)
         if (m == null)
-          nl.join(nr)
+          nl.join(t, nr)
         else
           mkNode(m, nl, nr)
       } else {
-        val (l, m, r) = this.split(b.key)
-        val nl = l.intersect(b.left)
-        val nr = r.intersect(b.right)
+        val (l, m, r) = this.split(t, b.key)
+        val nl = l.intersect(t, b.left)
+        val nr = r.intersect(t, b.right)
         if (m == null)
-          nl.join(nr)
+          nl.join(t, nr)
         else
           mkNode(b, nl, nr) // The b value has precendence over this value.
       }
   }
 
-  def diff(that: Node): Node = that match {
+  def diff(t: T, that: Node): Node = that match {
     case e: Empty => this
     case b: Full =>
       // TODO: Need to research alternative "diffb" algorithm from scandal...
       //       http://www.cs.cmu.edu/~scandal/treaps/src/treap.sml
       //
-      val (l2, m, r2) = b.split(key)
-      val l = left.diff(l2)
-      val r = right.diff(r2)
+      val (l2, m, r2) = b.split(t, key)
+      val l = left.diff(t, l2)
+      val r = right.diff(t, r2)
       if (m == null)
         mkNode(this, l, r)
       else
-        l.join(r)
+        l.join(t, r)
   }
 }
 
@@ -215,7 +216,7 @@ abstract class TreapFullNode[A <% Ordered[A], B] extends TreapNode[A, B]
 /**
  * An in-memory treap node implementation.
  */
-case class TreapMemNode[A <% Ordered[A], B](key: A, value: B, left: TreapNode[A, B], right: TreapNode[A, B]) 
+case class TreapMemNode[A <% Ordered[A], B <: AnyRef](key: A, value: B, left: TreapNode[A, B], right: TreapNode[A, B]) 
    extends TreapFullNode[A, B] 
 {
   def mkNode(basis: Full, left: Node, right: Node): Node = basis match {
@@ -230,7 +231,7 @@ case class TreapMemNode[A <% Ordered[A], B](key: A, value: B, left: TreapNode[A,
  * A treap node that's potentially stored to nonvolatile/persistent 
  * storage.  So, it's evictable from memory.
  */
-case class TreapStorableNode[A <% Ordered[A], B](
+case class TreapStorableNode[A <% Ordered[A], B <: AnyRef](
   key: A,
   keyInnerMin: A,
   keyInnerMax: A,
@@ -248,55 +249,57 @@ case class TreapStorableNode[A <% Ordered[A], B](
     key == keyInnerMin && 
     key == keyInnerMax
   
-  def value: B = inner.value
-  
-  def inner: TreapStorableNode[A, B] = synchronized {
+  def value(t: T): B                       = swizzleLoad(t).value
+  def inner(t: T): TreapStorableNode[A, B] = swizzleLoad(t).node
+
+  def swizzleLoad(t: T) = synchronized {
     // Swizzle/load from storage, if not already.
     //
     swizzle.synchronized {
       if (swizzle.node == null) {
       }
-      swizzle.node
     }
+    swizzle
   }
 
-  override def lookup(s: A): Node = 
+  override def lookup(t: T, s: A): Node = 
     if (s < keyInnerMin)
-      left.lookup(s)
+      left.lookup(t, s)
     else if (s > keyInnerMax)
-      right.lookup(s)
+      right.lookup(t, s)
     else 
-      inner.lookup(s)
+      inner(t).lookup(t, s)
 
-  override def split(s: A) = {
+  override def split(t: T, s: A) = {
     if (s < keyInnerMin) {
       if (isLeaf)
         (left, null, this) // Optimization when isLeaf.
       else {
-        val (l1, m, r1) = left.split(s)
+        val (l1, m, r1) = left.split(t, s)
         (l1, m, mkNode(this, r1, right))
       }
     } else if (s > keyInnerMax) {
       if (isLeaf)
         (this, null, right) // Optimization when isLeaf.
       else {
-        val (l1, m, r1) = right.split(s)
+        val (l1, m, r1) = right.split(t, s)
         (mkNode(this, left, l1), m, r1)
       }
     } else {
       // Split point "s" is without [keyInnerMin, keyInnerMax] range.
       //
-      val (l1, m, r1) = inner.split(s)
-      (left.join(l1), m, r1.join(right))
+      val (l1, m, r1) = inner(t).split(t, s)
+      (left.join(t, l1), m, r1.join(t, right))
     }
   }
 }
 
-class TreapStorageNodeSwizzle[A <% Ordered[A], B] {
+class TreapStorageNodeSwizzle[A <% Ordered[A], B <: AnyRef] {
   type Storable = TreapStorableNode[A, B]
 
   private var loc_i: Long      = -1L 
   private var node_i: Storable = null
+  private var value_i: B       = _
   
   def loc: Long = synchronized { loc_i }
   
@@ -324,23 +327,30 @@ class TreapStorageNodeSwizzle[A <% Ordered[A], B] {
     node_i = x; 
     this 
   }
+
+  def value: B = synchronized { value_i }
+  
+  def value_!!(x: B) = synchronized { 
+    value_i = x; 
+    this 
+  }
 }
 
 // ---------------------------------------------------------
 
 object TreapTest {
   def main(args: Array[String]) {
-    val e = TreapEmptyNode[Int, Int]
-    val t0 = new Treap[Int, Int]
+    val e = TreapEmptyNode[Int, String]
+    val t0 = new Treap[Int, String]
     println(t0)
     
-    val t1 = new Treap[Int, Int](TreapMemNode(1, 100, e, e))
+    val t1 = new Treap[Int, String](TreapMemNode(1, "100", e, e))
     println(t1)
 
-    val t2 = new Treap[Int, Int](TreapMemNode(2, 200, e, e))
+    val t2 = new Treap[Int, String](TreapMemNode(2, "200", e, e))
     println(t2)
     
-    val t1_1 = new Treap[Int, Int](TreapMemNode(1, 101, e, e))
+    val t1_1 = new Treap[Int, String](TreapMemNode(1, "101", e, e))
     println(t1_1)
 
     println(t1.union(t2))
@@ -350,7 +360,7 @@ object TreapTest {
     println(t1.diff(t2))
     println(t2.diff(t1))
     
-    val t3 = new Treap[Int, Int](TreapMemNode(3, 300, e, e))
+    val t3 = new Treap[Int, String](TreapMemNode(3, "300", e, e))
     println(t1.union(t2).union(t3))
     println(t1.union(t2).union(t3).intersect(t1.union(t2)))    
     println(t1.union(t2).union(t3).diff(t1.union(t2)))    
