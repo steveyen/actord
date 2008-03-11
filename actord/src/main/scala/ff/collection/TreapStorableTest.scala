f7449e7f16148755dce137bdcfcb3868a9d34936
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-03-11 17:20:18 +0000

    split out storage tests from treap tests

--- actord/src/main/scala/ff/collection/TreapStorableTest.scala
+++ actord/src/main/scala/ff/collection/TreapStorableTest.scala
@@ -27,86 +27,10 @@ import java.io._
  */
 class TreapStorableTest extends TestConsoleMain {
   def suite = new TestSuite(
-    ( "should be empty after create" ::
-      "should handle simple appends and reads" ::
-      "should handle loc append/read" ::
-      Nil
-    ).map(name => new SingleFileStorageTestCase(name)) :::  
     ( "should be empty after creation" ::
       Nil
     ).map(name => new TreapStorableTestCase(name)) :_*
   )
-}
-
-class SingleFileStorageTestCase(name: String) extends TestCase(name) {
-  override def runTest = {
-    println("test: " + name)
-    name match {
-      case "should be empty after create" =>
-        val f = File.createTempFile("test_sfs", ".tmp")
-        val s = new SingleFileStorage(f)
-        val loc = s.append((loc, appender) => appender.appendUTF("hello"))
-        assertEquals(loc, StorageLoc(0, 0L))
-
-        s.close        
-        f.delete
-        
-      case "should handle simple appends and reads" =>
-        val f = File.createTempFile("test_sfs1", ".tmp")
-        val s = new SingleFileStorage(f)
-        val loc = s.append((loc, appender) => appender.appendUTF("hello"))
-        assertEquals(loc, StorageLoc(0, 0L))
-        s.close
-
-        val s2 = new SingleFileStorage(f)
-        val b2 = s2.readAt(loc, _.readUTF)
-        assertEquals("hello", b2)
-        s2.close
-
-        val s3 = new SingleFileStorage(f)
-        val loc3 = s3.append((loc, appender) => appender.appendUTF("world"))
-        assertEquals(loc3, StorageLoc(0, 7L))
-        val loc4 = s3.append((loc, appender) => appender.appendUTF("there"))
-        assertEquals(loc4, StorageLoc(0, 14L))
-        
-        assertEquals("hello", s3.readAt(loc,  _.readUTF))
-        assertEquals("world", s3.readAt(loc3, _.readUTF))
-        assertEquals("there", s3.readAt(loc4, _.readUTF))
-        assertEquals("world", s3.readAt(loc3, _.readUTF))
-        assertEquals("there", s3.readAt(loc4, _.readUTF))
-        assertEquals("hello", s3.readAt(loc,  _.readUTF))
-        s3.close
-        
-        f.delete
-
-      case "should handle loc append/read" =>
-        val f = File.createTempFile("test_sfs2", ".tmp")
-        val s = new SingleFileStorage(f)
-
-        val loc1 = s.append((loc, appender) => appender.appendUTF("hello"))
-        val loc2 = s.append((loc, appender) => appender.appendUTF("world"))
-        val loc3 = s.append((loc, appender) => appender.appendUTF("there"))
-
-        assertEquals(loc1, StorageLoc(0, 0L))
-        assertEquals(loc2, StorageLoc(0, 7L))
-        assertEquals(loc3, StorageLoc(0, 14L))
-        
-        assertEquals("hello", s.readAt(loc1, _.readUTF))
-        assertEquals("world", s.readAt(loc2, _.readUTF))
-        assertEquals("there", s.readAt(loc3, _.readUTF))
-
-        val locA = s.append((loc, appender) => appender.appendLoc(loc1))
-        val locB = s.append((loc, appender) => appender.appendLoc(loc2))
-        val locC = s.append((loc, appender) => appender.appendLoc(loc3))        
-
-        assertEquals(loc1, s.readAt(locA, _.readLoc))
-        assertEquals(loc2, s.readAt(locB, _.readLoc))
-        assertEquals(loc3, s.readAt(locC, _.readLoc))
-
-        s.close        
-        f.delete
-    }
-  }
 }
 
 class TreapStorableTestCase(name: String) extends TestCase(name) {
--- actord/src/test/java/ff/collection/MainTest.java
+++ actord/src/test/java/ff/collection/MainTest.java
@@ -6,6 +6,7 @@ public class MainTest extends TestCase {
   public void testMain() {
     // Run actual tests via scala SUnit.
     //
+    new StorageTest().main(new String[0]); 
     new TreapStorableTest().main(new String[0]); 
   }
 }
