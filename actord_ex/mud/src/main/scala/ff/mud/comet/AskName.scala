5011336b4ec5efef1f2df21c6c195df41076f1e2
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-04-08 19:23:18 +0000

    added chat feature

---
+++ actord_ex/mud/src/main/scala/ff/mud/comet/AskName.scala
@@ -0,0 +1,16 @@
+package ff.mud.comet
+
+import net.liftweb.http._
+import S._
+import SHtml._
+import net.liftweb.util._
+import scala.xml._
+
+class AskName(initInfo: CometActorInitInfo) extends CometActor(initInfo) {
+  def defaultPrefix = "ask_name"
+
+  def render = ajaxForm(
+    <div>What is your username?</div> ++ 
+    text("", name => answer(name.trim)) ++
+    <input type="submit" value="Enter"/>)
+}
