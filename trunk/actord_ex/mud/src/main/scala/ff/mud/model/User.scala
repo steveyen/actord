acc51f92a9e0b93a55473e749065c8a0f224047f
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-04-08 00:54:44 +0000

    whitespace

--- trunk/actord_ex/mud/src/main/scala/ff/mud/model/User.scala
+++ trunk/actord_ex/mud/src/main/scala/ff/mud/model/User.scala
@@ -8,12 +8,16 @@ import net.liftweb.util._
  */
 object User extends User with MetaMegaProtoUser[User, User with KeyedMetaMapper[Long, User]] {
   override def dbTableName = "users" // define the DB table name
-  override def screenWrap = Full(<lift:surround with="default" at="content">
-			       <lift:bind /></lift:surround>)
+
+  override def screenWrap = Full(
+    <lift:surround with="default" at="content">
+      <lift:bind/>
+		</lift:surround>)
+
   // define the order fields will appear in forms and output
   override def fieldOrder = id :: firstName :: lastName :: email :: 
-  locale :: timezone ::
-  password :: textArea :: Nil
+                            locale :: timezone ::
+                            password :: textArea :: Nil
 
   // comment this line out to require email validations
   override def skipEmailValidation = true
@@ -24,11 +28,12 @@ object User extends User with MetaMegaProtoUser[User, User with KeyedMetaMapper[
  */
 class User extends MegaProtoUser[User] {
   def getSingleton = User // what's the "meta" server
+
   def primaryKeyField = id
   
   // define an additional field for a personal essay
   object textArea extends MappedTextarea(this, 2048) {
-    override def textareaRows  = 10
+    override def textareaRows = 10
     override def textareaCols = 50
     override def displayName = "Personal Essay"
   }
