39b165571e3e31418ab5cd899d97507f63ed7f39
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-04-29 07:39:11 +0000

    started a simple java blocking socket impl, to compare to mina performance

---
+++ trunk/actord/src/main/scala/ff/actord/ProtocolSimple.scala
@@ -0,0 +1,138 @@
+/**
+ * Copyright 2008 Steve Yen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ff.actord
+
+import java.io._
+import java.net._
+
+import org.slf4j._
+
+import ff.actord.Util._
+
+class SAcceptor(server: MServer, protocol: MProtocol, numProcessors: Int, port: Int) extends Thread {
+  override def run = {
+    var idNext = 0L
+    val ss     = new ServerSocket(port)
+    
+    while (true) {
+      (new SSession(server, protocol, ss.accept, idNext)).start
+      idNext = idNext + 1L
+    }
+  }
+}
+
+class SSession(server: MServer, protocol: MProtocol, s: Socket, id: Long) 
+  extends Thread 
+     with MBufferIn
+     with MBufferOut
+     with MSession {
+  s.setTcpNoDelay(true)
+  
+  val MIN_CMD_SIZE = "quit\r\n".length
+  
+  private var waitingFor = MIN_CMD_SIZE
+  private var available  = 0
+  private var readPos    = 0
+
+  private var buf = new Array[Byte](waitingFor)
+  private val is = s.getInputStream
+  private val os = s.getOutputStream
+  
+  private var numMessages = 0L
+
+  override def run = {
+    while (s.isClosed == false) {
+      val lastRead = is.read(buf, available, buf.length - available)
+      if (lastRead <= -1)
+        s.close
+        
+      available = available + lastRead
+      if (available > buf.length) {
+        s.close
+        throw new RuntimeException("available larger than buf somehow")
+      }
+
+      if (available >= waitingFor) {
+        val indexCR = buf.indexOf(CR)
+        if (indexCR < 0 ||
+            indexCR + CRNL.length > available) {
+          waitingFor = waitingFor + 1024
+          if (waitingFor > buf.length)
+            bufGrow(waitingFor)
+        } else {
+          readPos = 0
+          
+          val line = readString(indexCR + CRNL.length)
+          if (line.endsWith(CRNL) == false) {
+            s.close
+            throw new RuntimeException("missing CRNL")
+          } else {
+            val bytesNeeded = protocol.process(server, this, line, this, available)
+            if (bytesNeeded == 0) {
+              numMessages = numMessages + 1              
+
+              if (available > readPos)
+                Array.copy(buf, readPos, buf, 0, available - readPos)
+            
+              waitingFor = MIN_CMD_SIZE
+              available  = available - readPos
+              readPos    = 0
+            } else {
+              waitingFor = bytesNeeded
+              if (waitingFor > buf.length)
+                bufGrow(waitingFor)
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  def bufGrow(size: Int) = {
+    val bufPrev = buf
+    buf = new Array[Byte](size)
+    Array.copy(bufPrev, 0, buf, 0, bufPrev.length)
+  }
+     
+  def read(bytes: Array[Byte]): Unit = {
+    if (readPos + bytes.length > available)
+      throw new RuntimeException("reading more bytes than available: " + available)
+
+    Array.copy(buf, readPos, bytes, 0, bytes.length)
+    readPos = readPos + bytes.length
+  }
+  
+  def readString(num: Int): String = {
+    if (readPos + num > available)
+      throw new RuntimeException("reading more string than available: " + available)
+      
+    val r = new String(buf, readPos, num, "UTF-8")
+    readPos = readPos + num
+    r
+  } 
+  
+  def close: Unit = s.close
+  
+  def write(resList: List[MResponse]): Unit = {
+    for (res <- resList) 
+      res.put(this)
+  }
+  
+  def getReadMessages: Long = numMessages
+
+  def put(bytes: Array[Byte]): Unit = os.write(bytes)
+}
+
