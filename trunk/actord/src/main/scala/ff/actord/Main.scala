6e8c98e480d0a78032a338039081b34368b54dcb
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-05-02 16:56:04 +0000

    simple blocking protocol now works better than mina/grizzly nio frameworks

--- trunk/actord/src/main/scala/ff/actord/Main.scala
+++ trunk/actord/src/main/scala/ff/actord/Main.scala
@@ -24,7 +24,7 @@ object Main
     // new MainProgGrizzly().start(args)
     // new MainProgMina().start(args)
     
-    new MainProgMina().start(args)
+    new MainProgSimple().start(args)
   }
 }
 
