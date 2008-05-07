b205038977ace3b2ef9888dc0e26a9e4d68c653c
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-05-07 07:41:50 +0000

    trying to implement uniform and list buckets

--- actord/src/main/scala/ff/actord/client/Key.scala
+++ actord/src/main/scala/ff/actord/client/Key.scala
@@ -21,31 +21,56 @@ case class LeafNode(name: String, weight: Float, kind: String, info: String) ext
 trait BucketNode extends Node {
   def children: Seq[Node]
   def bucketType: String
+
+  // val sumOfWeights: Float = children.foldLeft(0.0)((accum, child) => accum + child.weight)
+
+  def crushHash32_2(name: String, x: Int): Int = 0 // TODO
+  def crushHash32_2(x: Int, name: String): Int = 0 // TODO
+  def crushHash32_4(x: Int, c: Node, r: Int, name: String): Int = 0 // TODO
 }
 
 case class ListBucket(name: String, weight: Float, kind: String, info: String, children: Seq[Node]) 
   extends BucketNode {
   def bucketType: String = "list"
-  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = children(0)
+  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = {
+    for (i <- 0 until children.length) {
+      val c = children(i)
+      var w = crushHash32_4(x, c, r, name)
+      w = w & 0xffff
+      // w = w * sumOfWeights
+      w = w >> 16
+      if (w < c.weight)
+        return c
+    }
+    null
+  }
 }
 
 case class TreeBucket(name: String, weight: Float, kind: String, info: String, children: Seq[Node]) 
   extends BucketNode {
   def bucketType: String = "tree"
-  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = children(0)
+  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = null // TODO
 }
 
 case class UniformBucket(name: String, weight: Float, kind: String, info: String, children: Seq[Node]) 
   extends BucketNode {
   def bucketType: String = "uniform"
-  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = children(0)
+  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = {
+    val o = crushHash32_2(x, name) & 0xffff
+    val p = primes(crushHash32_2(name, x).toInt % children.length)
+    val s = (x + o + (r + 1) * p) % children.length
+    children(s)
+  }
+  val primes: Seq[Int] = Nil // TODO
 }
 
 case class StrawBucket(name: String, weight: Float, kind: String, info: String, children: Seq[Node]) 
   extends BucketNode {
   def bucketType: String = "straw"
-  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = children(0)
+  def chooseChild(x: Int, r: Int, fTotal: Int, fLocal: Int, numReplicas: Int): Node = null // TODO
 }
+
+// -----------------------------------------------------------
 
 class NodeMap(tree: NodeTree, steps: Seq[Step]) {
   def findReplicas(x: Int, nodeStatusMap: Map[String, Float]): Seq[Node] = 
@@ -139,6 +164,8 @@ class NodeTree(root: Node) {
 
   def nodeOk(o: Node, nodeStatusMap: Map[String, Float], x: Int) = true
 }
+
+// -----------------------------------------------------------
 
 trait Step {
   def doStep(x: Int, t: NodeTree, working: Seq[Node], nodeStatusMap: Map[String, Float]): Seq[Node]
