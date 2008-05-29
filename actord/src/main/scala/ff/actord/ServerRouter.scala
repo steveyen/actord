91fae6f178ef7885c6d3d2af930e2655b90ce4d7
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-05-29 21:39:47 +0000

    refactored into separate server-proxy-simple file

--- actord/src/main/scala/ff/actord/ServerRouter.scala
+++ actord/src/main/scala/ff/actord/ServerRouter.scala
@@ -23,6 +23,9 @@ import java.net._
 import ff.actord.Util._
 import ff.actord.MProtocol._
 
+/**
+ * An MProtocol implementation that routes messages to downstream target server(s).
+ */
 trait MServerRouter extends MProtocol {
   def chooseTarget(spec: MSpec, clientSession: MSession, 
                    cmdArr: Array[Byte], cmdArrLen: Int, cmdLen: Int): MRouterTarget
@@ -77,20 +80,26 @@ trait MServerRouter extends MProtocol {
     processTargetResponse(target, clientSession, cmdArr, cmdArrLen)
   }
 
+  // -----------------------------------------------
+
   def processTargetResponse(target: MRouterTarget, clientSession: MSession, cmdArr: Array[Byte], cmdArrLen: Int): Int = {
     if (!noReply(cmdArr, cmdArrLen)) {
-      var r = clientSession.attachment.asInstanceOf[Response]
-      if (r == null) {
-          r = new Response(target, clientSession)
-          clientSession.attachment_!(r)
+      var m = clientSession.attachment.asInstanceOf[mutable.Map[MRouterTarget, Response]]
+      if (m == null) {
+          m = new mutable.HashMap[MRouterTarget, Response]
+          clientSession.attachment_!(m)
       }
+      var r = if (m.contains(target)) 
+                m(target) 
+              else {
+                val rr = new Response(target, clientSession)
+                m += (target -> rr)
+                rr
+              }
       r.go
     }
     GOOD
   }
-
-  val VALUEBytes   = stringToArray("VALUE ")
-  val NOREPLYBytes = stringToArray(" noreply" + CRNL)
 
   def noReply(cmdArr: Array[Byte], cmdArrLen: Int): Boolean = 
     if (cmdArrLen > NOREPLYBytes.length)
@@ -98,6 +107,9 @@ trait MServerRouter extends MProtocol {
                    cmdArr, cmdArrLen - NOREPLYBytes.length, NOREPLYBytes.length) == 0
     else
       false
+
+  val NOREPLYBytes = stringToArray(" noreply" + CRNL)
+  val VALUEBytes   = stringToArray("VALUE ")
 
   /**
    * Processes the response/reply from the server.
@@ -167,122 +179,10 @@ trait MRouterTarget {
   def write(m: String): Unit                               
   def write(a: Array[Byte]): Unit                          
   def write(a: Array[Byte], offset: Int, length: Int): Unit
-
   def writeFunc: (Array[Byte], Int, Int) => Unit
-
-  def isClosed: Boolean
+  def writeFlush: Unit
 
   def close: Unit
-
-  def writeFlush: Unit
+  def isClosed: Boolean
 }
-
-// --------------------------------------------------
-
-class SServerRouter(host: String, port: Int) extends MServerRouter { // A simple router.
-  val target = new SRouterTarget(host, port)                   
-
-  def chooseTarget(spec: MSpec, clientSession: MSession, 
-                   cmdArr: Array[Byte], cmdArrLen: Int, cmdLen: Int): MRouterTarget = target
-}
-
-// --------------------------------------------------
-
-class SRouterTarget(host: String, port: Int) extends MRouterTarget { // A simple router target.
-  def readResponse(buf: Array[Byte], offset: Int, length: Int): Int =
-    is.read(buf, offset, length)
-
-  protected var s  = new Socket(host, port) // The downstream target server to route to.
-  protected var is = s.getInputStream
-  protected var os = s.getOutputStream
-  protected var bs = new BufferedOutputStream(os)
-
-  def write(m: String): Unit                                = bs.write(stringToArray(m))
-  def write(a: Array[Byte]): Unit                           = bs.write(a, 0, a.length)
-  def write(a: Array[Byte], offset: Int, length: Int): Unit = bs.write(a, offset, length)
-
-  def writeFunc = (a: Array[Byte], offset: Int, length: Int) => bs.write(a, offset, length)
-
-  def isClosed = s == null
-
-  def close: Unit = {
-    if (s != null)
-        s.close
-    s  = null
-    is = null
-    os = null
-    bs = null
-  }
-
-  def writeFlush: Unit = 
-    try {
-      bs.flush
-    } catch {
-      case _ => close
-    }
-}
-
-  // unfinished: 
-  //  also can do a tee here, replicator, two-level cache
-  /*
-    proxyServer        = ServerProxy(Client(targetHost, targetPort))
-    loggingProxyServer = LoggingProxy(ServerProxy(Client(targetHost, targetPort)))
-    replicatingProxy   = ServerProxy(Client(targetHost, targetPort),
-                                     Client(targetHost, targetPort))
-
-    the localCache is the hard one...
-      need individual specs
-        read-thru get spec
-        write-thru set spec
-
-    localCache = ReadThruProxy(Client(targetHost, targetPort)))
-    localCache = WriteThruProxy(Client(targetHost, targetPort)))
-
-    Option[localServer]
-    List[remoteServer]
-    rules on how to choose between and process commands
-
-    if sync-write-thru and set
-      set into remote
-
-    if localServer
-      try local first
-      if miss and sync-read-thru
-        get from remote and add to local
-        return hit or miss
-
-   router matrix          synch            or async
-     write-thru-to-remote before or after  before or after
-     read-thru-to-remote  before or after  before or after
-
-   syncLevel is 0-sync, 1-sync, Quorum-sync, N-sync, All-sync
-
-   // By unified, we mean this pseudocode handles read and write ops.
-   //
-   // The primary might be the local store.
-   // The replicas might be 1 or more remote stores.
-   //
-   // Analytics and remote logging might be handled as just yet another replica?
-   //
-   def unifiedOp(...) = { 
-     var repNBefore = _
-     var repNAfter  = _
-
-     if (startReplicas == before)
-       repNBefore = startReplicas(syncLevel)
-
-     var pri = doPrimary
-
-     if (startReplicas == after &&
-         pri & repNBefore are not satisfied)
-       repNAfter = startReplicas(syncLevel)
-
-     wait until (pri & repNBefore & repNAfter are satisfied) or timeout
-   }
-
-   MServer is a server interface
-   MMainServer implements MServer, is local in-mem cache/server
-   MServerProxy implements MServer, forwards msgs to a single remote server at host:port
-   MServerRouter forwards to 1 or more downstream target servers
-  */
 
