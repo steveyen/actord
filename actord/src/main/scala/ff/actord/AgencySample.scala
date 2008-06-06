9a3139eeba54c3f3d8ea000bab6bc8833d760efc
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-06-06 18:27:32 +0000

    more agency sample usage

--- actord/src/main/scala/ff/actord/AgencySample.scala
+++ actord/src/main/scala/ff/actord/AgencySample.scala
@@ -7,16 +7,37 @@ import ff.actord._
 import ff.actord.Agency._
 
 object SampleUsingAgency {
-  def main(args: Array[String]) {
-  }
+  def main(args: Array[String]) {}
 
   val agency = new ActorDAgency("127.0.0.1", 11411)
 
   agency.localRegister(createActorCard, 
-    actor { loop { react {
-      case CreateActor(callee, msg, server) =>
-    } } }
+    actor { 
+      loop { 
+        react {
+          case CreateActor(callee, msg, server) => callee.base.split("/")(0) match {
+            case "invoice" =>
+              val c = Card(callee.base, "")
+              val a = new InvoiceActor(c)
+              // register(server, , new InvoiceActor(c)) // TODO!
+            case _ =>
+          }
+          case _ =>
+        }
+      }
+    }
   )
 
   Agency.init(agency)
+}
+
+class InvoiceActor(myCard: Card) extends Actor {
+  def act {
+    loop {
+      react {
+        case _ =>
+      }
+    }
+  }
+  start
 }
