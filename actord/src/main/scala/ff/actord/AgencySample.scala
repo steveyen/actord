ebbf718f70eefde161792c343b6fcd06d1162f70
steve.yen <steve.yen@eb686281-4247-0410-9daf-e591f29f01f7>
2008-06-08 07:34:46 +0000

    taking port params in chat samples

--- actord/src/main/scala/ff/actord/AgencySample.scala
+++ actord/src/main/scala/ff/actord/AgencySample.scala
@@ -9,49 +9,52 @@ import ff.actord.Agency._
 // Example of an ActorD Agency based chat room server and client.
 //
 // Start a chat server...
-//   scala -cp target/classes ff.actord.ChatRoomServer
+//   scala -cp target/classes ff.actord.ChatRoomServer port
 //
 object ChatRoomServer {
-  def main(args: Array[String]) {}
+  var agency: Agency = null
 
-  // Starts this server process listening on port 11411...
-  //
-  val agency = new ActorDAgency("127.0.0.1", 11411)
+  def main(args: Array[String]) {
+    val port = Integer.parseInt(args(0))
 
-  // Register a local actor that can create other actors.
-  // Remote clients can ask this server to create some
-  // chatRoom's by doing...
-  //
-  agency.localRegister(createActorCard, 
-    actor { 
-      loop { 
-        react {
-          case CreateActor(cardBase, args, pool) => 
-            println("CreateActor " + cardBase)
-            val splitArr = cardBase.split("/")
+    // Starts this server process listening on port...
+    //
+    val agency = new ActorDAgency("127.0.0.1", port)
 
-            splitArr(0) match {
-              case "chatRoom" =>
-                args match {
-                  case AddChatRoom(roomTitle, caller) =>
-                    val r = new ChatRoom(Card(cardBase, ""), roomTitle)
-                    val o = pool.offer(r.myCard, r, false)
-                    if (o == true)
-                        r.start
-                    if (caller != null)
-                        caller ~> Reply(myCard, args, r.myCard)
-                    println("AddChatRoom: " + cardBase + " for caller: " + caller + " offer: " + o)
-                  case _ =>
-                }
-              case _ =>
-            }
-          case _ =>
+    // Register a local actor that can create other actors.
+    // Remote clients can ask this server to create some
+    // chatRoom's by doing...
+    //
+    agency.localRegister(createActorCard, 
+      actor { 
+        loop { 
+          react {
+            case CreateActor(cardBase, args, pool) => 
+              println("CreateActor " + cardBase)
+              val splitArr = cardBase.split("/")
+ 
+              splitArr(0) match {
+                case "chatRoom" =>
+                  args match {
+                    case AddChatRoom(roomTitle, caller) =>
+                      val r = new ChatRoom(Card(cardBase, ""), roomTitle)
+                      val o = pool.offer(r.myCard, r, false)
+                      if (o == true)
+                          r.start
+                      if (caller != null)
+                          caller ~> Reply(myCard, args, r.myCard)
+                      println("AddChatRoom: " + cardBase + " for caller: " + caller + " offer: " + o)
+                    case _ =>
+                  }
+                case _ =>
+              }
+            case _ =>
+          }
         }
-      }
-    }
-  )
+      })
 
-  Agency.initDefault(agency)
+    Agency.initDefault(agency)
+  }
 }
 
 case class AddChatRoom(roomTitle: String, caller: Card)
@@ -83,27 +86,34 @@ case class ChatRoomView(viewer: Card)
 //   scala -cp target/classes ff.actord.ChatRoomClient [roomKey] [userId] [some-single-word-msg]
 //
 object ChatClient {
-  // Starts the local process listening on port 11422...
-  //
-  Agency.initDefault(new ActorDAgency("127.0.0.1", 11422) {
-    override def nodeForIndirect(c: Card): Node = {
-      // Hardcode our expected one and only server Node in this example.
-      // Normally, we'd instead do some consistent-hashing or CRUSH here.
-      //
-      return Node("127.0.0.1", 11411)
-    }
-  })
-
-  case class ChatClientGo
-
   def main(args: Array[String]) {
-    if (args.length != 3) {
-      println("usage: scala ff.actord.ChatRoomClient roomKey userId msg")
+    if (args.length != 4) {
+      println("usage: scala ff.actord.ChatRoomClient serverPort0,serverPort1 roomKey userId msg")
       return
     }
 
-    val roomBase = "chatRoom/" + args(0)
+    val serverPorts: Array[Int] = args(0).split(',').map(Integer.parseInt _)
+    if (serverPorts.length < 1) {
+      println("need at least 1 serverPort")
+      return
+    }
+
+    // Starts the local process listening on port 11422...
+    //
+    Agency.initDefault(new ActorDAgency("127.0.0.1", 11422) {
+      override def nodeForIndirect(c: Card): Node = {
+        // Hardcode our expected one and only server Node in this example.
+        // Normally, we'd instead do some consistent-hashing or CRUSH here.
+        //
+        return Node("127.0.0.1", serverPorts(c.base.hashCode % serverPorts.length))
+      } 
+    })
+
+    val roomKey  = args(1)
+    val roomBase = "chatRoom/" + roomKey
     var roomCard = Card(roomBase, "")
+    val userId   = args(2)
+    val msg      = args(3)
 
     val u = actor {
       loop {
@@ -111,15 +121,15 @@ object ChatClient {
           case ChatClientGo =>
             // Create chat room, if not already...
             //
-            createActorCard(roomBase) ~> AddChatRoom("room " + args(0) + " is fun!", myCard)
+            createActorCard(roomBase) ~> AddChatRoom("room " + roomKey + " is fun!", myCard)
 
           case Reply(_, AddChatRoom(_, _), newRoomCard: Card) => 
             roomCard = newRoomCard
-            self ! args(2)
+            self ! msg
 
           case text: String =>
             println("sending... text: " + text)
-            roomCard ~> ChatRoomMessage(args(1), System.currentTimeMillis, text)
+            roomCard ~> ChatRoomMessage(userId, System.currentTimeMillis, text)
             roomCard ~> ChatRoomView(myCard)
             println("sending... done")
 
@@ -136,4 +146,5 @@ object ChatClient {
   }
 }
 
+case class ChatClientGo
 
